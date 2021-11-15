e4160ed11f3bc3d68273df409763a37c57ddf2d2
Lycorisz <1229656469@qq.com>
2021-11-15 17:46:02 +0800

    初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    theme: 'reco',
+    themeConfig: {
+       // 博客配置
+      type: 'blog',
+      subSidebar: 'auto',
+      authorAvatar: '/avatar.jpg',
+      blogConfig: {
+        category: {
+          location: 2,     // 在导航栏菜单中所占的位置，默认2
+          text: '分类' // 默认文案 “分类”
+        },
+        tag: {
+          location: 3,     // 在导航栏菜单中所占的位置，默认3
+          text: 'Tag'      // 默认文案 “标签”
+        },
+        socialLinks: [     // 信息栏展示社交信息
+          { icon: 'reco-github', link: 'https://github.com/Lycorisz/mo' }
+          // { icon: 'reco-npm', link: 'https://www.npmjs.com/~reco_luan' }
+        ]
+      },
+      nav: [
+        {
+          text: 'Home',
+          link: '/',
+          icon: 'reco-tag'
+        },
+        {
+          text: 'Vue',
+          items: [
+            {
+              text: 'Vue基础使用',
+						  link: '/Vue/Vue_base'
+            }
+          ]
+        },
+      ]
+    }
+  }
---
+++ docs/README.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroImage: /KND.PNG
+heroImageStyle: {
+  maxWidth: '100%',
+  display: block,
+  boxShadow: '0 5px 18px rgba(0,0,0,0.2)'
+}
+heroText: null
+tagline: null
+actionText: 开始学习
+actionLink: /Vue/
+footer: MIT Licensed | Copyright © 2021-present Lycorisz
+---
